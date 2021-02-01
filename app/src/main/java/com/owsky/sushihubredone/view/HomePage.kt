42ffb651583346a2ad5ea867c297e8e6e727c3a8
Nicolò Bertocco <nick.bertocco@gmail.com>
2021-02-01 19:10:07 +0100

    Implemented homepage

--- app/src/main/java/com/owsky/sushihubredone/view/HomePage.kt
+++ app/src/main/java/com/owsky/sushihubredone/view/HomePage.kt
@@ -1,24 +1,24 @@
-package com.owsky.sushihubredone
+package com.owsky.sushihubredone.view
 
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.DialogFragment
-import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import com.owsky.sushihubredone.R
 
 class HomePage : Fragment(R.layout.fragment_homepage) {
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
-		view.findViewById<Button>(R.id.btnJoin).setOnClickListener { _ -> findNavController().navigate(R.id.action)}
-		view.findViewById<Button>(R.id.btnCreate).setOnClickListener { _ -> findNavController().navigate(R.id.action)}
+		view.findViewById<Button>(R.id.btnJoin).setOnClickListener { _ -> findNavController().navigate(
+			R.id.action_homePageNav_to_scanQRNav)}
+		view.findViewById<Button>(R.id.btnCreate).setOnClickListener { _ -> findNavController().navigate(
+			R.id.action_homePageNav_to_configureTableNav)}
 
 		val prefs = requireActivity().getPreferences(Context.MODE_PRIVATE)
 		prefs.getString("table_code", null)?.let {
@@ -31,7 +31,7 @@ class HomePage : Fragment(R.layout.fragment_homepage) {
 
 		override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 			val builder = AlertDialog.Builder(requireContext())
-			builder.setTitle("Do you want to access the last unfinished session?")
+			builder.setTitle("Do you want to resume the last unfinished session?")
 				.setPositiveButton("Yes") { _, _ -> findNavController().navigate(R.id.listsNav) }
 				.setNegativeButton("No") {_, _ -> {
 					// TODO: implement viewmodel checkout
